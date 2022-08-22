aabe0bfc8f3b0eb2709754de07234acbbba8af6e
ethanbar11 <ethanbarbar@gmail.com>
2022-08-22 10:49:51 +0300

    ff

--- socket_server_example.py
+++ socket_server_example.py
@@ -1,31 +1,50 @@
 import socket
+import threading
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
-# The maximum is 65,535 (2^16 -1)
+MAX_MESSAGE_LENGTH = 1024
 
-# I'm creating here a socket.
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Binding this socket to a specific port.
-s.bind((HOST, PORT))
+class Server:
+    def __init__(self):
+        HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+        PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+        self.listening_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.listening_socket.bind((HOST, PORT))
+        self.client_socket = None
+        self.connected_to_client = False
 
-# Start listening to incoming requests.
-s.listen()
+    def send_to_client(self, message):
+        # If this is False it throws an exception.
+        assert self.connected_to_client
+        self.client_socket.sendall(message.encode('utf-8'))
 
-print('Im starting to listen please connect me')
+    def receiving_from_client(self):
+        while self.connected_to_client:
+            message = self.client_socket.recv(MAX_MESSAGE_LENGTH).decode('utf-8')
+            if message is None:
+                self.connected_to_client = False
+            else:
+                print('CLIENT: ' + message)
 
-# Blocking function meaning until a client is going to connect to the server
-# I'm going to be stuck here.
+    def listen(self):
+        self.listening_socket.listen()
+        self.client_socket, addr = self.listening_socket.accept()
+        self.connected_to_client = True
 
-conn, addr = s.accept()
+        # I'm opening here a thread that is going to receive messages from the client
+        # all the time until the connection is closed.
+        t = threading.Thread(target=self.receiving_from_client)
+        t.start()
 
-with conn:
-    print(f"Connected by {addr}")
-    while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-        conn.sendall(data)
+    def close(self):
+        self.client_socket.close()
+        self.listening_socket.close()
 
-s.close()
+
+if __name__ == '__main__':
+    server = Server()
+    server.listen()
+    while server.connected_to_client:
+        message = input('Please enter a message to send to the client: ')
+        server.send_to_client(message)
+    server.close()
